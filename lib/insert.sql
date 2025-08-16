ea51a245d35af50f1b77455ca155f32c0810a364
Daniel Muhia <muhiadan17@gmail.com>
2025-08-16 17:31:09 +0300

    compltes lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 15, 'M', 'brown', 'calm', 1),
+(2, 'Rowdy', 8, 'M', 'black', 'rowdy', 1),
+(3, 'Tabitha', 5, 'F', 'brown', 'gentle', 1),
+(4, 'Sergeant Brown', 12, 'M', 'brown', 'strict', 0),
+(5, 'Melissa', 10, 'F', 'black', 'playful', 1),
+(6, 'Grinch', 3, 'M', 'green', 'grumpy', 1),
+(7, 'Wendy', 7, 'F', 'brown', 'sweet', 1),
+(8, NULL, 20, 'M', 'black', 'aggressive', 0);
